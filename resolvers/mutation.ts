5e36da4afcc38cbce57cfe3de9109d181db56866
Ignacio García <ijgarciaest@gmail.com>
2024-06-29 20:09:30 +0200

    modified:   resolvers/mutation.ts

--- resolvers/mutation.ts
+++ resolvers/mutation.ts
@@ -2,5 +2,18 @@ import { Modelopokemon } from "../db/pokemons.ts";
 
 
 export const Mutation={
-    
+
+    deletePokemon:async(_:unknown,
+        args:{id:string}
+    )=>{
+        try {
+           const poke= await Modelopokemon.findByIdAndDelete(args.id)
+            if(poke==null){
+                throw new Error()
+            }
+            return true
+        } catch (error) {
+            return false
+        }
+    }
 }
